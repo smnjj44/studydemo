0beb7a459961eadd6664a0bf08edeb8ecb0863c9
NameForSimon <565587783@qq.com>
2020-05-14 11:02:49 +0800

    springbootlogger

---
+++ springbootlogger/src/main/java/com/study/springbootlogger/SpringbootloggerApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootloggerApplication.class, args);
+    }
+
+}
---
+++ springbootlogger/src/test/java/com/study/springbootlogger/SpringbootloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbootlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
