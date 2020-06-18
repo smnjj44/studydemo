103fc435e77dd9af6bc8f0ec883880e9e1bc1dcd
NameForSimon <565587783@qq.com>
2020-06-18 19:05:22 +0800

    开始rabbitmqdemo

---
+++ rabbitmq/src/main/java/com/study/rabbitmq/utils/ConnectionUtil.java
@@ -0,0 +1,12 @@
+package com.study.rabbitmq.utils;
+
+/**
+ * @author Simon
+ * @version 1.0.0
+ * @Description
+ * @ClassName ConnectionUtil
+ * @date 2020.06.18 17:38
+ */
+public class ConnectionUtil {
+
+}
