594f4d2be9d5f1517230e3683e5d081dbf835256
simon <565587783@qq.com>
2020-09-02 17:05:11 +0800

    增加切面缓存注解

---
+++ tlcache/src/test/java/com/study/tlcache/TlcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.tlcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
